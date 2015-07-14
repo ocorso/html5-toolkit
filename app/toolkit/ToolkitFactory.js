2aa84fce0a11dacd9e70a6fe82b91ad65365c7f3
Michael Misciagna <mmisciagna@google.com>
2015-07-14 10:45:06 -0700

    comment out search bar; pull in toolkit data from local json file downloaded from Firebase

--- app/toolkit/ToolkitFactory.js
+++ app/toolkit/ToolkitFactory.js
@@ -2,11 +2,13 @@
  * Firebase toolkit items
  */
 toolkit.factory('ToolkitData', function($http) {
-  var url = 'https://html5-toolkit.firebaseio.com/.json';
+  // var url = 'https://html5-toolkit.firebaseio.com/.json';
+  var url = '../../json/html5-toolkit-export.json';
   var toolkit = {};
 
   toolkit.items = function() {
-    return $http.jsonp(url + '?callback=JSON_CALLBACK');
+    // return $http.jsonp(url + '?callback=JSON_CALLBACK');
+    return $http.get(url);
   };
 
   return toolkit;
